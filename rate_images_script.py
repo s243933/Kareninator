d11a6213a57ca3a6c9efaab9c4c66b838a8859e5
srde <s243933@dtu.dk>
2025-09-11 09:26:25 +0200

    resize images and new script

--- rate_images_script.py
+++ rate_images_script.py
@@ -1,66 +1,74 @@
-import pandas as pd
 import os
+import random
+import pandas as pd
 import streamlit as st
 
+# FOLDER = r"/Users/fredhb/Desktop/project folder/Kareninator/data/preprocessed_data"
+FOLDER = r"C:\Users\desus\OneDrive - Danmarks Tekniske Universitet\Fall_2025\Cognitive_Modelling\Kareninator\data\preprocessed_data"
+RANDOM_SEED = 22
 
+# ---- init ----
+if "images" not in st.session_state:
+    paths = [os.path.join(FOLDER, f) for f in os.listdir(FOLDER)]
+    valid = [p for p in paths if os.path.splitext(p)[1].lower() in {".jpg",".jpeg",".png",".webp",".bmp"}]
+    # duplicate each image once
+    doubled = valid * 2
+    random.Random(RANDOM_SEED).shuffle(doubled)
+    st.session_state.images = doubled
 
-col1,col2 = st.columns(2)
-
-def showPhoto(photo, pathsImages):
-    filename = os.path.basename(photo)
-    col1.subheader(f"{st.session_state.counter+1}/{len(pathsImages)}")
-    col1.image(photo, caption=f"{filename}")
-    # col1.write(f"Index as a session_state attribute: {st.session_state.counter}")
-    
-    ## Increments the counter to get next photo
-    st.session_state.counter += 1
-    if st.session_state.counter >= len(pathsImages):
-        st.write("You have rated all images. Thank you!")
-        st.session_state.counter = len(pathsImages) - 1  # Prevent going out of bounds
-
-
-def store_rating(photo, rating, pathsImages):
-    st.session_state['current_rating'] = rating
-    st.session_state['current_photo'] = photo
-
-    st.session_state['data'].append({"Image": os.path.basename(photo), "Rating": rating})
-
-    showPhoto(photo, pathsImages)
-
-    
-
-# Get list of images in folder
-folderWithImages = r"C:\Users\desus\OneDrive - Danmarks Tekniske Universitet\Fall_2025\Cognitive_Modelling\Kareninator\data"
-pathsImages = [os.path.join(folderWithImages,f) for f in os.listdir(folderWithImages)]
-
-# col1.subheader("List of images in folder")
-# col1.write(pathsImages)
-
-if 'counter' not in st.session_state:
-    col1.write("Click the button to start the rating process...")
+if "counter" not in st.session_state:
     st.session_state.counter = 0
+if "ratings" not in st.session_state:
+    # list of dicts so we can store multiple ratings per image
+    st.session_state.ratings = []
 
-# Select photo a send it to button
-photo = pathsImages[st.session_state.counter]
-col2.subheader("Rate the image")
+images = st.session_state.images
+if not images:
+    st.error("No images found.")
+    st.stop()
 
-def get_data():
-    if 'data' not in st.session_state:
-        st.session_state['data'] = []
-    return st.session_state['data']
+idx = max(0, min(st.session_state.counter, len(images)-1))
+st.session_state.counter = idx
+photo = images[idx]
+filename = os.path.basename(photo)
 
-# rating = col2.text_input("Rating (0-5)")
-rating = col2.slider("foo", 0, 5)
+col1, col2 = st.columns(2)
+col1.subheader(f"{idx+1}/{len(images)}")
+col1.image(photo, caption=filename, use_container_width=True)
+col2.write("#")
 
+# Unique slider key per "instance" of image (filename + position in list)
+slider_key = f"rating_{idx}_{filename}"
 
+# If the slider key hasn't been created before, pre-seed it with 0
+if slider_key not in st.session_state:
+    st.session_state[slider_key] = 0
 
-show_btn = col2.button("Show next pic ⏭️",on_click=store_rating,args=([photo, rating, pathsImages]))
+rating = col2.slider("Rating (0–5)", 0, 5, key=slider_key)
+col2.caption("Use the slider, then click a button below.")
 
-# if show_btn and 0 <= int(rating) <= 5:
-#     photoname = os.path.basename(photo)
-#     get_data().append({"Image": photoname, "Rating": rating})
-# else:
-#     col2.warning("Please enter a valid rating between 0 and 5.")
+# Save & Next
+if col2.button("Save rating & next ⏭️"):
+    st.session_state.ratings.append({"Image": filename, "Rating": int(rating)})
+    if st.session_state.counter < len(images) - 1:
+        st.session_state.counter += 1
+        st.rerun()
+    else:
+        st.success("You have rated all images twice. 🎉")
 
-rating_df = pd.DataFrame(get_data())
-print(rating_df)
+# Back
+if col2.button("⬅️ Back"):
+    if st.session_state.counter > 0:
+        st.session_state.counter -= 1
+        st.rerun()
+
+# Table + download
+rating_df = pd.DataFrame(st.session_state.ratings)
+st.divider()
+st.dataframe(rating_df, use_container_width=True)
+st.download_button(
+    "⬇️ Download CSV",
+    data=rating_df.to_csv(index=False).encode("utf-8"),
+    file_name="image_ratings.csv",
+    mime="text/csv"
+)
